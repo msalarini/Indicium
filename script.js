ace1e223bc467b87b7d644132a3f05496c0ea1ae
Marcus Salarini <72809258+msalarini@users.noreply.github.com>
2021-04-07 18:36:53 -0300

    Update script.js
    atualização

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSetion(event){
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-  
-  section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
+  function scrollToSetion(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  } 
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSetion)
   });
-} 
-
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollToSetion)
-})
+}
