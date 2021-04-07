8c7932647a5ab164b02ce4c8abb10b66e174cf1f
Marcus <marcussalarini@gmail.com>
2021-04-06 21:33:36 -0300

    desafio terminado

---
+++ script.js
@@ -0,0 +1,16 @@
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSetion(event){
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+} 
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSetion)
+})
